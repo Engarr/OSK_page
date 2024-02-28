547d1e292d33a58d181892cd73141ccd03b898b0
Łukasz <lukasz.poczta12@gmail.com>
2024-02-28 16:28:25 +0100

    modify infoBox animation

--- components/home-page/welcome-section/welcome-card/welcome-card.tsx
+++ components/home-page/welcome-section/welcome-card/welcome-card.tsx
@@ -14,7 +14,7 @@ const WelcomeCard = () => {
         className='bg-[var(--black-80)] flex flex-col md:flex-row xl:gap-10 px-2 py-2 rounded-md mt-3 xs:mt-10 md:mt-[100px]'
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ type: 'spring', stiffness: 100, delay: 0.5 }}>
+        transition={{ type: 'tween', delay: 0.5 }}>
         {moreInfo.map((i, index) => (
           <React.Fragment key={index}>
             <InfoBox img={i.img} text={i.text} />
