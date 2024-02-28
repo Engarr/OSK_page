99098d98a675bf3475cfb2ececbd54df8d945146
Łukasz <lukasz.poczta12@gmail.com>
2024-02-28 16:48:00 +0100

    modify infoBox animation

--- components/home-page/welcome-section/welcome-card/info-box/info-box.tsx
+++ components/home-page/welcome-section/welcome-card/info-box/info-box.tsx
@@ -8,14 +8,14 @@ type InfoBox = {
 
 const InfoBox = ({ img, text }: InfoBox) => {
   return (
-    <div className='flex items-center '>
+    <div className='flex items-center justify-start'>
       <Image
         src={img}
         alt={text}
         className='w-[20px] md:w-[30px] lg:w-auto mx-1'
         priority
       />
-      <h3 className='text-[var(--text-white-1)] text-xs lg:text-base ml-1 font-semibold lg:tracking-wider '>
+      <h3 className='text-[var(--text-white-1)] text-xs lg:text-base ml-1 font-semibold lg:tracking-wider xs:p-1 '>
         {text}
       </h3>
     </div>
--- components/home-page/welcome-section/welcome-card/welcome-card.tsx
+++ components/home-page/welcome-section/welcome-card/welcome-card.tsx
@@ -11,10 +11,10 @@ const WelcomeCard = () => {
       <Title />
 
       <motion.div
-        className='bg-[var(--black-80)] flex flex-col md:flex-row xl:gap-10 px-2 py-2 rounded-md mt-3 xs:mt-10 md:mt-[100px]'
-        initial={{ opacity: 0, y: -20 }}
+        className='bg-[var(--black-80)] flex flex-col md:flex-row xl:gap-10 gap-1 p-2 rounded-md mt-3 xs:mt-10 md:mt-[100px] opacity-0'
+        initial={{ opacity: 0, y: -5 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ type: 'tween', delay: 0.5 }}>
+        transition={{ delay: 0.5, duration: 0.3 }}>
         {moreInfo.map((i, index) => (
           <React.Fragment key={index}>
             <InfoBox img={i.img} text={i.text} />
