6eb0b28cd0ff2aa5c6b2553bc09b3e1eb7ccc216
Łukasz <lukasz.poczta12@gmail.com>
2024-03-11 16:09:10 +0100

    modify gallery

--- lib/gallery-images.ts
+++ lib/gallery-images.ts
@@ -3,6 +3,8 @@ import carTwo from '@/public/images/gallery/cars_2.jpg';
 import carThree from '@/public/images/gallery/cars_3.jpg';
 import carFour from '@/public/images/gallery/cars_4.jpg';
 import carFive from '@/public/images/gallery/cars_5.jpg';
+import carSix from '@/public/images/gallery/cars_6.jpg';
+import carSeven from '@/public/images/gallery/cars_7.jpg';
 
 import officeOne from '@/public/images/gallery/office_1.jpg';
 import officeTwo from '@/public/images/gallery/office_2.jpg';
@@ -13,10 +15,12 @@ import maneuveringAreaOne from '@/public/images/gallery/maneuvering_area_1.jpg';
 import maneuveringAreaTwo from '@/public/images/gallery/maneuvering_area_2.jpg';
 import maneuveringAreaThree from '@/public/images/gallery/maneuvering_area_3.jpg';
 import maneuveringAreaFour from '@/public/images/gallery/maneuvering_area_4.jpg';
+import maneuveringAreaFive from '@/public/images/gallery/maneuvering_area_5.jpg';
+import maneuveringAreaSix from '@/public/images/gallery/maneuvering_area_6.jpg';
 
 export const carsGallery = {
   title: 'Samochody',
-  imgArr: [carThree, carFour, carFive, carTwo],
+  imgArr: [carThree, carFour, carFive, carTwo, carSix, carSeven],
 };
 export const officeGallery = {
   title: 'Biuro',
@@ -29,5 +33,7 @@ export const maneuveringAreaGallery = {
     maneuveringAreaTwo,
     maneuveringAreaFour,
     maneuveringAreaOne,
+    maneuveringAreaFive,
+    maneuveringAreaSix,
   ],
 };
